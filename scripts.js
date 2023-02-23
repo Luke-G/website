f3150bcf335a96f10859f0f41a63e110a54df990
Luke Gorman <luke@lukegorman.co.uk>
2023-02-23 12:01:08 +1100

    fix: background

--- scripts.js
+++ scripts.js
@@ -1,10 +1,8 @@
-window.onload = function () {
-  VANTA.CLOUDS({
-    el: "#background",
-    mouseControls: true,
-    touchControls: true,
-    gyroControls: false,
-    minHeight: 200.0,
-    minWidth: 200.0,
-  });
-};
+VANTA.CLOUDS({
+  el: "#background",
+  mouseControls: true,
+  touchControls: true,
+  gyroControls: false,
+  minHeight: 200.0,
+  minWidth: 200.0,
+});
