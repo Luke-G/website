0fbedb1575fd3b3ac8d76cd1f4c9e78c02a20e3f
Luke Gorman <luke@lukegorman.co.uk>
2023-02-23 12:12:11 +1100

    chore: try vanta clouds 2

--- scripts.js
+++ scripts.js
@@ -1,8 +1,10 @@
-VANTA.CLOUDS({
+VANTA.CLOUDS2({
   el: "#background",
   mouseControls: false,
   touchControls: false,
   gyroControls: false,
   minHeight: 200.0,
   minWidth: 200.0,
+  scale: 1.0,
+  texturePath: "noise.png",
 });
